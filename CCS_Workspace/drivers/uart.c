0aec4847cb8ca168b88a35887b596b9739de73a9
Connor Troy <connor.troy@colorado.edu>
2019-02-15 12:25:53 -0700

    Revert "Hacked some pieces together"
    This reverts commit 55ed11132a2328f385df0a4144daeb8d6e20d6fb.

--- CCS_Workspace/drivers/uart.c
+++ CCS_Workspace/drivers/uart.c
@@ -7,6 +7,7 @@
  * This driver handles UART communication
  */
 
+#include "msp.h"
 #include "uart.h"
 
 /*
@@ -89,9 +90,9 @@ Enables the EUSCI Module
 */
 void uart_enable(EUSCI_A_Type * module)
 {
-    EUSCI_A_CMSIS(module) -> CTLW0 &= ~EUSCI_B_CTLW0_SWRST;
-    EUSCI_A_CMSIS(module) -> IFG &= ~(EUSCI_A_IFG_RXIFG); // Clear Receive flag
-    EUSCI_A_CMSIS(module) -> IE |= EUSCI_A_IE_RXIE; // Enable USCI_A0 Receive Interrupt
+    EUSCI_A_CMSIS(module)->CTLW0 &= ~EUSCI_B_CTLW0_SWRST;
+    EUSCI_A_CMSIS(module) -> IFG &= ~(EUSCI_A_IFG_RXIFG); // Clear receive flag
+    EUSCI_A_CMSIS(module) -> IE |= EUSCI_A_IE_RXIE; // Enable USCI_A0 Receive character Interrupt
 }
 
 
@@ -109,20 +110,12 @@ void uart_send_byte(EUSCI_A_Type * module, uint8_t data)
 /*
 Transmit String (or similar) over UART
 */
-void uart_send_multiple(EUSCI_A_Type * module, uint8_t * data, uint32_t num_bytes)
+void uart_send_multple(EUSCI_A_Type * module, uint8_t * data, uint32_t num_bytes)
 {
     uint32_t count = 0;
     while (count < num_bytes)
     {
-        if (data[count] != '\0')
-        {
-            uart_send_byte( module, data[count++] );
-        }
-        else
-        {
-            uart_send_byte( module, '\n' );
-            count++;
-        }
+        uart_send_byte( module, data[count++] );
     }
 }
 
--- CCS_Workspace/sandbox/driverConfig.h
+++ CCS_Workspace/sandbox/driverConfig.h
@@ -15,9 +15,6 @@
 
 #include "msp.h"
 
-#include <stdint.h>
-#include <stdio.h>
-
 /* Protocol Drivers */
 #include "i2c.h"
 #include "uart.h"
@@ -25,9 +22,8 @@
 /* Device Drivers */
 #include "ina219.h"
 
-/* Source Files */
-#include "source/ext_connect.h"
-
+#include <stdint.h>
+#include <string.h>
 
 #define TMP_ADDR                0x48
 #define ACC_ADDR_1              0x68
@@ -37,9 +33,8 @@
 #define I2C_MODULE              EUSCI_B1
 #define UART_MODULE             EUSCI_A0
 
-
-#define UART_RX_BUF_LEN         15
-uint8_t UART_RX_BUF_MULTIBYTE[UART_RX_BUF_LEN];
+const uint32_t  UART_RX_BUF_LEN = 10;
+      uint8_t   UART_RX_BUF_MULTIBYTE[UART_RX_BUF_LEN];
 
 /* I2C Master Configuration Parameter */
 const I2C_Config i2cConfig =
